7a2447a824d6367813832aabbd895881cde0cc9d
x123rtyizzz <x123rtyizzz@gmail.com>
2021-11-10 17:18:20 -0300

    Remove unnecesary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("CLient side javascript")
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
